294997946e5d8fda9fe78a9a17992f6ba2e2a8b7
Riu <josemanuelriu@gmail.com>
2026-01-13 13:47:27 +0100

    Add query for professors without assigned subjects

--- queries/queries.sql
+++ queries/queries.sql
@@ -85,7 +85,11 @@ LEFT JOIN profesor pr ON pr.id_departamento=d.id
 WHERE pr.id_profesor IS NULL;
 
 -- 13. Retorna un llistat amb els professors/es que no imparteixen cap assignatura. (apellido1, apellido2, nombre)
-
+SELECT DISTINCT p.apellido1, p.apellido2, p.nombre
+FROM persona p
+RIGHT JOIN profesor pr ON pr.id_profesor=p.id
+LEFT JOIN asignatura a ON a.id_profesor=pr.id_profesor
+WHERE a.id is NULL;
 
 -- 14. Retorna un llistat amb les assignatures que no tenen un professor/a assignat. (id, nombre)
 
