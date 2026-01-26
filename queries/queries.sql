7ed6a494ddd9c17a982939dea7e9c8a5228535e6
Riu <josemanuelriu@gmail.com>
2026-01-26 18:03:05 +0100

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -144,7 +144,7 @@ GROUP BY g.nombre, a.tipo;
 -- 23. Retorna un llistat que mostri quants alumnes s'han matriculat d'alguna assignatura en cadascun dels cursos escolars. El resultat haurà de mostrar dues columnes, una columna amb l'any d'inici del curs escolar i una altra amb el nombre d'alumnes matriculats. (anyo_inicio, total)
 SELECT ce.anyo_inicio, COUNT(DISTINCT am.id_alumno) AS total
 FROM curso_escolar ce
-RIGHT JOIN alumno_se_matricula_asignatura am ON am.id_asignatura=ce.id
+JOIN alumno_se_matricula_asignatura am ON am.id_curso_escolar=ce.id
 GROUP BY ce.anyo_inicio;
 
 -- 24. Retorna un llistat amb el nombre d'assignatures que imparteix cada professor/a. El llistat ha de tenir en compte aquells professors/es que no imparteixen cap assignatura. El resultat mostrarà cinc columnes: id, nom, primer cognom, segon cognom i nombre d'assignatures. El resultat estarà ordenat de major a menor pel nombre d'assignatures. (id, nombre, apellido1, apellido2, total)
